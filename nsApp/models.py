3ffe60cf23eeb9b64528fb07d19acf7985f0d56f
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2022-11-28 18:54:59 -0600

    Se define función __str__

--- nsApp/models.py
+++ nsApp/models.py
@@ -5,6 +5,9 @@ class Author(models.Model):
     firstName=models.CharField(max_length=20)
     lastName=models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.firstName + " " + self.lastName
+
 class Book(models.Model):
     title = models.CharField(max_length=20)
     ratings=models.CharField(max_length=10)
